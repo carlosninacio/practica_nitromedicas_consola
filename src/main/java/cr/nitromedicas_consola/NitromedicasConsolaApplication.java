4f14ca736fab680dbec50dff3e66f8d32601a81a
carlosninacio <cigramirez@poligran.edu.co>
2026-01-08 18:32:28 -0500

    Base app SpringBoot

---
+++ src/main/java/cr/nitromedicas_consola/NitromedicasConsolaApplication.java
@@ -0,0 +1,13 @@
+package cr.nitromedicas_consola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitromedicasConsolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NitromedicasConsolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cr/nitromedicas_consola/NitromedicasConsolaApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.nitromedicas_consola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NitromedicasConsolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
